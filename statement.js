2626f752661a9f58a5e4a2a505f5d353c0c82a2d
3014zhangshuo <3014zhangshuo@gmail.com>
2019-07-13 23:09:14 +0800

    分离出数据层程序

--- statement.js
+++ statement.js
@@ -1,23 +1,28 @@
 module.exports = function (invoice, plays) {
-  return renderPlainText(invoice, plays)
-}
+  const statementData = {}
+  statementData.customer = invoice.customer
+  statementData.performances = invoice.performances.map(enrichPerformance)
+  statementData.totalAmount = totalAmount(statementData)
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData)
+  return renderPlainText(statementData, plays)
 
-function renderPlainText(invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`
-
-  for (let perf of invoice.performances) {
-    // print line for this order
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
+  function enrichPerformance(aPerformance) {
+    // perform shadow copy, don't modify the data passed into the function
+    const result = Object.assign({}, aPerformance)
+    result.play = playFor(result)
+    result.amount  = amountFor(result)
+    result.volumeCredits = volumeCreditsFor(result)
+    return result
+  }
+  
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
   }
 
-  result += `Amount owed is ${usd(totalAmount())}\n`
-  result += `You earned ${totalVolumeCredits()} credits\n`
-  return result
-
-  function amountFor (aPerformance) {
+  function amountFor(aPerformance) {
     let result = 0
 
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
     case "tragedy":
       result = 40000
       if (aPerformance.audience > 30) {
@@ -38,39 +43,50 @@ function renderPlainText(invoice, plays) {
     return result
   }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID]
-  }
-
   function volumeCreditsFor(aPerformance) {
     let result = 0
     result += Math.max(aPerformance.audience - 30, 0)
     // add extra credit for every ten comedy attendees
-    if ("comedy" == playFor(aPerformance).type) {
+    if ("comedy" == aPerformance.play.type) {
       result += Math.floor(aPerformance.audience / 5)
     }
 
     return result
   }
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", 
-      { style: "currency", currency: "USD", maximumFractionDigits: 2 }).format(aNumber / 100)
-  }
-
-  function totalVolumeCredits() {
-    let result = 0
-    for (let perf of invoice.performances) {
-      result += volumeCreditsFor(perf)
-    }
-    return result
-  }
 
   function totalAmount() {
     let result = 0
-    for (let perf of invoice.performances) {
-      result += amountFor(perf)
+    for (let perf of data.performances) {
+      result += perf.amount
     }
     return result
+  }
+
+
+  function totalVolumeCredits() {
+    let result = 0
+    for (let perf of data.performances) {
+      result += perf.volumeCredits
+    }
+    return result
+  }
+}
+
+function renderPlainText(data) {
+  let result = `Statement for ${data.customer}\n`
+
+  for (let perf of data.performances) {
+    // print line for this order
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`
+  }
+
+  result += `Amount owed is ${usd(data.totalAmount)}\n`
+  result += `You earned ${data.totalVolumeCredits} credits\n`
+  return result
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", 
+      { style: "currency", currency: "USD", maximumFractionDigits: 2 }).format(aNumber / 100)
   }
 }
